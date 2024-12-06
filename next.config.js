7ebfb6f1b5c207ddb8950314e66292e9ab2c6f2e
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-12-06 17:29:34 +0530

    add initial Tauri setup with build configuration, main application entry, and default permissions

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
+const internalHost = process.env.TAURI_DEV_HOST || 'localhost';
 const nextConfig = {
   output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  assetPrefix: isProd ? undefined : `http://${internalHost}:3000`,
 };
 
 module.exports = nextConfig;
